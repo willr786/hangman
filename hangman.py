ef8008e116964dc685741d7cc7b9c887dde19372
willr786 <willr_786@yahoo.com>
2018-06-13 22:20:55 +0000

    My first commit.

---
+++ hangman.py
@@ -0,0 +1,45 @@
+def hangman(word):
+	"""This is for a game of Hangman."""
+	wrong = 0
+	stages = ["",
+			 "_________			",
+			 "|				",
+			 "|		    |	        ",
+			 "|	            0	        ",
+			 "|		   /|\		",
+			 "|		   / \		",
+			 "|				",
+			 ]
+	rletters = list(word)
+	board = ["__"] * len(word)
+	win = False
+	print("Welcome to Hangman")
+	
+	while wrong < len(stages) - 1:
+		print('\n')
+		msg = "Guess a letter: "
+		char = input(msg)
+		if char in rletters:
+			cind = rletters\
+					.index(char)
+			board[cind] = char
+			rletters[cind] = '$'
+		else:
+			wrong += 1
+		print((' '.join(board)))
+		e = wrong + 1
+		print('\n'
+				.join(stages[0:e]))
+		if "__" not in board:
+			print("You Win!")
+			print(' '.join(board))
+			win = True
+			break
+	if not win:
+		print('\n'
+				.join(stages[0: \
+				wrong]))
+		print("You lose! It was {}."
+			  .format(word))
+	
+hangman('football')
